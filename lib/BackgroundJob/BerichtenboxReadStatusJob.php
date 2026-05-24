095c65fe400d01dae6a882e64c7eb9f2ceadd185
Ruben van der Linde <ruben@conduction.nl>
2026-05-24 23:37:46 +0200

    retrofit: reverse-spec berichtenbox-integration (5 REQs / 5 files)
    REQ-001 Berichtenbox send/list/poll REST endpoints,
    REQ-002 BSN 11-proef + plain-text message validation,
    REQ-003 Pluggable Berichtenbox adapter contract,
    REQ-004 Read-status polling with 7-day unread-flagging,
    REQ-005 Daily background polling job.
    
    Ghost change: retrofit-2026-05-24-berichtenbox-integration.
    
    Refs #565.

--- lib/BackgroundJob/BerichtenboxReadStatusJob.php
+++ lib/BackgroundJob/BerichtenboxReadStatusJob.php
@@ -16,6 +16,8 @@
  * @version GIT: <git-id>
  *
  * @link https://procest.nl
+ *
+ * @spec openspec/changes/retrofit-2026-05-24-berichtenbox-integration/tasks.md#task-5
  */
 
 declare(strict_types=1);
--- lib/Service/BerichtenboxService.php
+++ lib/Service/BerichtenboxService.php
@@ -16,6 +16,9 @@
  * @version GIT: <git-id>
  *
  * @link https://procest.nl
+ *
+ * @spec openspec/changes/retrofit-2026-05-24-berichtenbox-integration/tasks.md#task-2
+ * @spec openspec/changes/retrofit-2026-05-24-berichtenbox-integration/tasks.md#task-4
  */
 
 declare(strict_types=1);
