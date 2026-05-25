1294cb0cf83812ce3dca789b989e4ff62e66a473
Ruben van der Linde <ruben@conduction.nl>
2026-05-25 09:57:20 +0200

    retrofit: reverse-spec parafering-actions general controller (3 REQs / 3 files) (#576)
    REQ-101 ParaferingController surface, REQ-102 ParaferingService lifecycle,
    REQ-103 ParaferingNotificationService notifications.
    
    Ghost change: retrofit-2026-05-24-parafering-actions-impl.
    
    Refs #565.

--- lib/Service/ParaferingService.php
+++ lib/Service/ParaferingService.php
@@ -17,6 +17,8 @@
  * @version GIT: <git-id>
  *
  * @link https://procest.nl
+ *
+ * @spec openspec/changes/retrofit-2026-05-24-parafering-actions-impl/tasks.md#task-2
  */
 
 declare(strict_types=1);
